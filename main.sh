eea947476d3af1d314975064fa940f1cceee637e
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-08-04 19:12:30 -0300

    Initial commit

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 declare -ra targets=(
 	'mips64el-unknown-linux-android21'
 	'i686-unknown-linux-android14'
@@ -12,21 +14,29 @@ declare -ra targets=(
 )
 
 declare cflags='-O2 -fno-math-errno'
-declare ldflags='-Xlinker --no-undefined'
+declare ldflags32='-Xlinker --no-undefined'
+declare ldflags64="${ldflags32} -l m"
 
 for triplet in "${targets[@]}"; do
 	declare name="${triplet::-2}"
 	declare tarball_filename="${name}.tar.xz"
+	declare ldflags="${ldflags32}"
+	
+	if [ "${name}" = 'mips64el-unknown-linux-android' ] || [ "${name}" = 'x86_64-unknown-linux-android' ] || [ "${name}" = 'riscv64-unknown-linux-android' ] || [ "${name}" = 'aarch64-unknown-linux-android' ]; then
+		ldflags="${ldflags64}"
+	fi
 	
 	"${triplet}-gcc" -g -c ${cflags} 'math.c'
 	"${triplet}-ar" rcs 'libpino-math.a' 'math.o'
 	
 	"${triplet}-gcc" ${ldflags} -shared 'math.o' -o 'libpino-math.so'
 	
-	"${triplet}-gcc" -g -c ${cflags} 'mmap64.c'
-	"${triplet}-ar" rcs 'libpino-mman.a' 'mmap64.o'
-	
-	"${triplet}-gcc" ${ldflags} -shared 'mmap64.o' -o 'libpino-mman.so'
+	if [ "${name}" = 'i686-unknown-linux-android' ] || [ "${name}" = 'armv7-unknown-linux-androideabi' ] || [ "${name}" = 'armv5-unknown-linux-androideabi' ] || [ "${name}" = 'mipsel-unknown-linux-android' ]; then
+		"${triplet}-gcc" -g -c ${cflags} 'mmap64.c'
+		"${triplet}-ar" rcs 'libpino-mman.a' 'mmap64.o'
+		
+		"${triplet}-gcc" ${ldflags} -shared 'mmap64.o' -o 'libpino-mman.so'
+	fi
 	
 	tar --create --file=- 'libpino-'* | xz --compress -9 > "${tarball_filename}"
 	sha256sum "${tarball_filename}" > "${tarball_filename}.sha256"
