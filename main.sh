6a344705d54a385d6e3c4f2f9f0ad2bb4874bd3b
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-08-04 14:14:18 -0300

    Initial commit

--- main.sh
+++ main.sh
@@ -11,7 +11,8 @@ declare -ra targets=(
 	'aarch64-unknown-linux-android21'
 )
 
-declare cflags='-O2 -fno-math-errno -Xlinker --no-undefined'
+declare cflags='-O2 -fno-math-errno'
+declare ldflags='-Xlinker --no-undefined'
 
 for triplet in "${targets[@]}"; do
 	declare name="${triplet::-2}"
@@ -20,12 +21,12 @@ for triplet in "${targets[@]}"; do
 	"${triplet}-gcc" -g -c ${cflags} 'math.c'
 	"${triplet}-ar" rcs 'libpino-math.a' 'math.o'
 	
-	"${triplet}-gcc" -shared 'math.o' -o 'libpino-math.so'
+	"${triplet}-gcc" ${ldflags} -shared 'math.o' -o 'libpino-math.so'
 	
 	"${triplet}-gcc" -g -c ${cflags} 'mmap64.c'
 	"${triplet}-ar" rcs 'libpino-mman.a' 'mmap64.o'
 	
-	"${triplet}-gcc" -shared 'mmap64.o' -o 'libpino-mman.so'
+	"${triplet}-gcc" ${ldflags} -shared 'mmap64.o' -o 'libpino-mman.so'
 	
 	tar --create --file=- 'libpino-'* | xz --compress -9 > "${tarball_filename}"
 	sha256sum "${tarball_filename}" > "${tarball_filename}.sha256"
